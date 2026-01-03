75c288bb8e3a6c7bb9d9041338afde9ca9a85221
nabasalim <salimnaba445@gmail.com>
2026-01-03 14:08:57 +0400

    Modified loop iteration logic

--- career_recommendation.py
+++ career_recommendation.py
@@ -4,9 +4,11 @@
 def classify_career_readiness(students):
     results = []
 
-    for student in students:
-        name = student["name"]
-        gpa = student["gpa"]
+    for i in range(len(students)):
+    student = students[i]
+    name = student["name"]
+    gpa = student["gpa"]
+
 
         if gpa >= 3.0:
             readiness = "High"
